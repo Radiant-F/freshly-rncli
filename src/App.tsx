5d97c32c9bc994b660c2ad649fad1f46bbd9accd
Radiant Fadilah <radiantfadilah0@gmail.com>
2024-12-19 09:40:59 +0700

    ruh roh

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux';
 import {store} from './redux/store';
 
 export default function App() {
-  console.log('pushing something from a branch other than main branch doesnt count as contribution?')
+  console.log('ruh roh')
   return (
     <Provider store={store}>
       <NavigationContainer>
