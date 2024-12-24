201c665874942840755ac044e4aaa7de9098817e
Radiant Fadilah <radiantfadilah0@gmail.com>
2024-12-24 08:43:33 +0700

    update log

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux';
 import {store} from './redux/store';
 
 export default function App() {
-  console.log('ruh roh')
+  console.log('not in my project')
   return (
     <Provider store={store}>
       <NavigationContainer>
