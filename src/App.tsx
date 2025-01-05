bdff213324ff91ed5b7f62dbf7238e7f99380772
Radiant Fadilah <radiantfadilah0@gmail.com>
2025-01-06 01:10:26 +0700

    update log

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux';
 import {store} from './redux/store';
 
 export default function App() {
-  console.log('happy new year!')
+  console.log('feeling meh')
   return (
     <Provider store={store}>
       <NavigationContainer>
