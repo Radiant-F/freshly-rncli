24396bb6cd0de9765d5c04180f8867eeb434c85a
Radiant Fadilah <radiantfadilah0@gmail.com>
2025-01-08 10:17:55 +0700

    update log

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux';
 import {store} from './redux/store';
 
 export default function App() {
-  console.log('testing commit 4')
+  console.log('testing commit 5')
   return (
     <Provider store={store}>
       <NavigationContainer>
